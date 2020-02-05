679f0ffcdcb2f2bba46632f11228a1e780527ab5
lladdy <randallboland@bigpond.com>
2020-02-06 09:22:28 +1030

    Add kingslayer command

--- main.go
+++ main.go
@@ -199,7 +199,63 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 			RefreshAllBotAuthorRoles()
 			s.ChannelMessageSend(m.ChannelID, "Roles refreshed.")
 		}
+
+		if method == "kingslayer" {
+			_, _, day, hour, min, _ := diff(viper.GetTime("meleechampionbotsince"), time.Now())
+			s.ChannelMessageSend(
+				"Current Kingslayer: " + viper.GetString("meleechampionbotname") +
+					"\nDuration: " + day + "d " + hour + "h " + min + "m")
+
+		}
 	}
+}
+
+// From https://golangr.com/difference-between-two-dates/
+func date_diff(a, b time.Time) (year, month, day, hour, min, sec int) {
+	if a.Location() != b.Location() {
+		b = b.In(a.Location())
+	}
+	if a.After(b) {
+		a, b = b, a
+	}
+	y1, M1, d1 := a.Date()
+	y2, M2, d2 := b.Date()
+
+	h1, m1, s1 := a.Clock()
+	h2, m2, s2 := b.Clock()
+
+	year = int(y2 - y1)
+	month = int(M2 - M1)
+	day = int(d2 - d1)
+	hour = int(h2 - h1)
+	min = int(m2 - m1)
+	sec = int(s2 - s1)
+
+	// Normalize negative values
+	if sec < 0 {
+		sec += 60
+		min--
+	}
+	if min < 0 {
+		min += 60
+		hour--
+	}
+	if hour < 0 {
+		hour += 24
+		day--
+	}
+	if day < 0 {
+		// days in month:
+		t := time.Date(y1, M1, 32, 0, 0, 0, 0, time.UTC)
+		day += 32 - t.Day()
+		month--
+	}
+	if month < 0 {
+		month += 12
+		year--
+	}
+
+	return
 }
 
 type BotInfoStruct struct {
